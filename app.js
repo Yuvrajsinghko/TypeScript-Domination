3c5aad0377a61d00382f4fb96fdf121541545c97
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-28 20:06:56 +0530

    datatypes in typescript

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+let b = [4, 5, 6];
+//tuple
+//fixed type and length
+let c = [12, 'yuvraj', true];
+//ENUMS
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super-admin";
+})(UserRoles || (UserRoles = {}));
+//Any,Unknown,Void,Null,Undefined,Never
+let a;
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-var a=4;
+let b:number[]=[4,5,6]
+
+//tuple
+//fixed type and length
+let c:[number,string,boolean]=[12,'yuvraj',true]
+
+//ENUMS
+enum UserRoles{
+    ADMIN="admin",
+    GUEST="guest",
+    SUPER_ADMIN="super-admin"
+}
+//Any,Unknown,Void,Null,Undefined,Never
+
+let a;
+
+
