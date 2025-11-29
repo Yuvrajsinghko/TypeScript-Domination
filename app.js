5758d45bf58eb710cf8b05c1670e926bf7fbbc6f
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-29 15:37:48 +0530

    updated app.ts file

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 "use strict";
-let b = [4, 5, 6];
-//tuple
-//fixed type and length
-let c = [12, "yuvraj", true];
-//ENUMS
-var UserRoles;
-(function (UserRoles) {
-    UserRoles["ADMIN"] = "admin";
-    UserRoles["GUEST"] = "guest";
-    UserRoles["SUPER_ADMIN"] = "super-admin";
-})(UserRoles || (UserRoles = {}));
-//Any,Unknown,Void,Null,Undefined,Never
-let a;
-let e;
-e = 12;
-e = "yuvraj";
-e = true;
-function abcd() {
-    console.log("hii");
-}
-function acd() {
-    return 45;
+// let b: number[] = [4, 5, 6];
+// //tuple
+// //fixed type and length
+// let c: [number, string, boolean] = [12, "yuvraj", true];
+// //ENUMS
+// enum UserRoles {
+//   ADMIN = "admin",
+//   GUEST = "guest",
+//   SUPER_ADMIN = "super-admin",
+// }
+// //Any,Unknown,Void,Null,Undefined,Never
+// let a;
+// let e: unknown;
+// e = 12;
+// e = "yuvraj";
+// e = true;
+// function abcd(): void {
+//   console.log("hii");
+// }
+// function acd(): number {
+//   return 45;
+// }
+// //type Inference
+// let t = 44.5;
+// // Type Annotations
+// let bo: number | boolean | string;
+// bo = 45.2;
+// bo = "ty";
+// bo = false;
+//Interfaces and Type Aliases
+function ab(a, b) {
+    console.log("hi");
 }
--- app.ts
+++ app.ts
@@ -1,26 +1,48 @@
-let b: number[] = [4, 5, 6];
+// let b: number[] = [4, 5, 6];
 
-//tuple
-//fixed type and length
-let c: [number, string, boolean] = [12, "yuvraj", true];
+// //tuple
+// //fixed type and length
+// let c: [number, string, boolean] = [12, "yuvraj", true];
 
-//ENUMS
-enum UserRoles {
-  ADMIN = "admin",
-  GUEST = "guest",
-  SUPER_ADMIN = "super-admin",
+// //ENUMS
+// enum UserRoles {
+//   ADMIN = "admin",
+//   GUEST = "guest",
+//   SUPER_ADMIN = "super-admin",
+// }
+// //Any,Unknown,Void,Null,Undefined,Never
+
+// let a;
+// let e: unknown;
+// e = 12;
+// e = "yuvraj";
+// e = true;
+
+// function abcd(): void {
+//   console.log("hii");
+// }
+// function acd(): number {
+//   return 45;
+// }
+// //type Inference
+
+// let t = 44.5;
+// // Type Annotations
+
+// let bo: number | boolean | string;
+// bo = 45.2;
+// bo = "ty";
+// bo = false;
+
+//Interfaces and Type Aliases
+
+function ab(a:number,b:string){
+    console.log("hi");
+    
 }
-//Any,Unknown,Void,Null,Undefined,Never
 
-let a;
-let e: unknown;
-e = 12;
-e = "yuvraj";
-e = true;
-
-function abcd(): void {
-  console.log("hii");
+interface User{
+    name:string,
+    email:string
 }
-function acd(): number {
-  return 45;
-}
+function getUserData(obj:)
