9ab68ff24b1b8eb3d131cca88a336d2ade4013e6
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-03 23:48:26 +0530

    added a type script file to understant the concept of unsin and intersection

---
+++ TypeScript-Main/src/unionAndany.ts
@@ -0,0 +1,13 @@
+// Union AND any
+
+let subs: number | string = 10;
+let apiRequestStatus: "pending" | "success" | "error" = "pending";
+
+apiRequestStatus = "success";
+
+const orders = ["12", "44", "6"];
+
+let currentOrders:string | undefined;
+
+
+
